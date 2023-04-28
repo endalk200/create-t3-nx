0dc68c09c551de0a1fef6ce1c2934d9d4b84d6e0
endalk200 <eb808826@gmail.com>
2023-04-28 13:24:18 +0300

    chore: updated next config file, removed unused css file, updated tsconfig to include new lib and configured projects.json for build output

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,5 +1,5 @@
 //@ts-check
-import '../../libs/utils/src/lib/env/env.mjs';
+import '../../libs/env-validator/src/lib/env.mjs';
 
 import { withNx } from '@nrwl/next/plugins/with-nx.js';
 
@@ -7,6 +7,18 @@ import { withNx } from '@nrwl/next/plugins/with-nx.js';
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  reactStrictMode: true,
+
+  /**
+   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+   * out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
@@ -14,8 +26,4 @@ const nextConfig = {
   },
 };
 
-export default withNx({
-  enabled: process.env.ANALYZE_BUNDLE === 'true',
-  openAnalyzer: true,
-  ...nextConfig,
-});
+export default withNx(nextConfig);
